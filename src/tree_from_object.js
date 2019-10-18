8ae8e87e4292e7d13e5c48442fff0e6201972bb2
Vitalii Starodub <Vitalii.Starodub@playtech.com>
2019-10-18 10:29:02 +0300

    'solution_vitalii'

---
+++ src/tree_from_object.js
@@ -0,0 +1,37 @@
+let container = document.getElementById('container');
+let data = {
+    "Рыбы": {
+        "форель": {},
+        "лосось": {}
+    },
+
+    "Деревья": {
+        "Огромные": {
+            "секвойя": {},
+            "дуб": {}
+        },
+        "Цветковые": {
+            "яблоня": {},
+            "магнолия": {}
+        }
+    }
+};
+
+function treeToObj(container, data) {
+    container.append(treeFromObject(data));
+}
+
+function treeFromObject(obj) {
+    let ul = document.createElement('ul');
+    for (let key in obj) {
+        let li = document.createElement('li');
+        li.textContent = key;
+        li.append(treeFromObject(obj[key]));
+        ul.append(li);
+    }
+
+    return ul;
+}
+
+treeToObj(container, data);
+
